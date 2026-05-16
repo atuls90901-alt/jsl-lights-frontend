271095bd8e742f2d398051310d61058b06d2e1d9
Atul <atuls90901@gmail.com>
2026-05-16 17:35:31 +0530

    production api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+ baseURL:
+"https://jsl-lights-backend.onrender.com/api",
 });
 
 axiosInstance.interceptors.request.use(
